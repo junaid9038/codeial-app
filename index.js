1fc4232d659531e1eee76ca47ab49cd8a2350e7c
junaid9038 <junaidahmed9038@gmail.com>
2023-02-18 13:00:37 +0530

    First commit:create the directary structure and setup express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app =express();
+const port = 8000;
+
+
+app.listen(port , function(err){
+    if(err){
+        console.log(`Error in the runing server :${port}`);
+    }// interpolation in JavaScript is a process in which an expression is inserted or placed in the string
+    console.log(`server runing in the port: ${port}`);
+});
